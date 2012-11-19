0e4e9fa0ca6aa6d7e71cff637e8f669841ead32f
mohit sharma <mohit.786.sharma@gmail.com>
2012-11-18 18:59:40 -0600

    added random forests

---
+++ machine learning/BaggedNRandomForests/myRForest.m
@@ -0,0 +1,127 @@
+%{
+   CSci5525 Fall'12 Homework 3
+   login: sharm163@umn.edu
+   date: 11/17/2012
+   name: Mohit Sharma
+   id: 4465482
+   algorithm: learn random forest classifier for given data and cross ...
+           validate it
+%}
+
+
+function[testError] = myRForest(dataFileName, randFeatureSetSizes, ...
+                                numFolds, numLayers)
+
+randFeatureSetSizes = randFeatureSetSizes(:);                            
+                            
+%load the data set
+load(dataFileName);
+
+%randomly shuffle data
+randInd = randperm(size(data, 1));
+permData = data(randInd, :);
+permLabels = labels(randInd, :);
+
+%get size of dataset
+sizeData = size(permData,1);
+
+%size of data to be left for validation
+leftDataSize = int16(sizeData/numFolds);
+
+%store mean error for each random feature set
+randFeatureTrainErrPcs = zeros(size(randFeatureSetSizes, 1), 1);
+randFeatureTestErrPcs = zeros(size(randFeatureSetSizes, 1), 1);
+
+for randFeatureSetIter=1:size(randFeatureSetSizes, 1)
+    
+    %size of fetaure set to be used to decide
+    sizeFeatureSet = randFeatureSetSizes(randFeatureSetIter);    
+    
+    %store accuracy of learned parameters for each validation
+    trainErrorPcs = zeros(numFolds, 1);
+    testErrorPcs = zeros(numFolds, 1);
+    
+    for iter=1:numFolds
+        validStart = (iter-1) * leftDataSize + 1;
+        validEnd = validStart + leftDataSize - 1;
+        if validEnd > sizeData
+            validEnd = sizeData;
+        end
+        
+        validationData = permData(validStart:validEnd, :);
+        validationLabels = permLabels(validStart:validEnd, :);
+    
+        if validEnd ~= sizeData 
+            trainingData = [permData(1:validStart-1, :); permData(validEnd+1: ...
+                                                              sizeData, :)];       
+            trainingLabels = [permLabels(1:validStart-1); permLabels(validEnd+1: ...
+                                                              sizeData, :)];
+        else
+            trainingData = permData(1:validStart-1, :);       
+            trainingLabels = permLabels(1:validStart-1);       
+        end
+    
+        %learn the random forest Classifier and get the training
+        %and test error
+        %TODO: write rForest
+        [trainErr, testErr] = rForest(trainingData, trainingLabels,...
+                                    validationData, validationLabels, ...
+                                    sizeFeatureSet, numLayers);
+    
+        testErrorPcs(iter) = testErr;
+        trainErrorPcs(iter) = trainErr;
+    end
+
+    fprintf('\n*************************************************\n');
+    fprintf('\nNo. of Base Classifier: %d\n', sizeFeatureSet);
+    %trainErrorPcs
+    fprintf('\nTrain Errors: \n');
+    for iter=1:size(trainErrorPcs, 1)
+        fprintf('%d\n', trainErrorPcs(iter));
+    end
+    
+    meanTrainErr = mean(trainErrorPcs);
+    stdTrainErr = std(trainErrorPcs);
+    fprintf('\nMean train error is %d\n', meanTrainErr);
+    fprintf('\nStandard deviation in train error is %d', stdTrainErr);
+    fprintf('\n');
+    
+    %testErrorPcs
+    fprintf('\nTest Errors: \n');
+    for iter=1:size(testErrorPcs, 1)
+        fprintf('%d\n', testErrorPcs(iter));
+    end
+    
+    meanTestErr = mean(testErrorPcs);
+    stdTestErr = std(testErrorPcs);
+    fprintf('\nMean test error is %d', meanTestErr);
+    fprintf('\nStandard deviation in test error is %d', stdTestErr);
+    fprintf('\n');
+    
+    %store the learned mean error and test error
+    randFeatureTrainErrPcs(randFeatureSetIter) = meanTrainErr;
+    randFeatureTestErrPcs(randFeatureSetIter) = meanTestErr;
+end
+
+%set return value
+testError = randFeatureTestErrPcs;
+
+fprintf('\nSize of Features\tTrainError\tTestError');
+for randFeatureSetIter=1:size(randFeatureSetSizes, 1)
+    fprintf('\n%d\t%d\t%d', randFeatureSetSizes(randFeatureSetIter), ...
+            randFeatureTrainErrPcs(randFeatureSetIter), ...
+            randFeatureTestErrPcs(randFeatureSetIter));
+    
+end
+
+%plot FeatureSetSizes vs training errors 
+plot(randFeatureSetSizes, randFeatureTrainErrPcs);
+xlabel('Size of Features');
+ylabel('Training Error');
+title('Random Forests');
+pause
+%plot FeatureSetSizes vs test errors
+plot(randFeatureSetSizes, randFeatureTestErrPcs);
+xlabel('Size of Features');
+ylabel('Test Error');
+title('Random Forests');
---
+++ machine learning/BaggedNRandomForests/rForest.m
@@ -0,0 +1,82 @@
+%{
+   CSci5525 Fall'12 Homework 3
+   login: sharm163@umn.edu
+   date: 11/17/2012
+   name: Mohit Sharma
+   id: 4465482
+   algorithm: learn bagging classifier for given training data and ...
+           apply it on test set data
+%}
+
+function [trainErrorPc, testErrorPc] = rForest(trainingData, trainingLabels,...
+                                             testData, testLabels, ...
+                                               sizeFeatureSet, ...
+                                               depth)
+
+
+    %size of taining data
+    sizeTrainData = size(trainingData, 1);
+    
+    %get valid attributes list for data
+    eligibleAttribs = [1:size(trainingData, 2)];
+    
+    %number of bootstrapped samples to learn classifiers, not
+    %specified in question using 10 as default
+    numBaseClassifier = 10;
+    
+    %store learned classifier in array
+    baggedClassifiers = Node.empty(numBaseClassifier, 0);
+    
+    %learn base classifiers
+    for baseClassIter=1:numBaseClassifier
+        %build bootstrap data 
+        [bootstrapData, origIdx] = datasample(trainingData, ...
+                                              size(trainingData, 1)); 
+        %get corresponding bootstrap labels
+        bootstrapLabels = trainingLabels(origIdx);
+        
+        %learn binary split decision tree of given depth on bootstrapped data
+        startDepth = 0;
+        isWeighted = 0;
+        dataWeights = ones(size(bootstrapData, 1), 1)/size(bootstrapData, 1);
+        isRandomForest = 1;
+        learnedTreeRoot = learnBinDtree(bootstrapData, bootstrapLabels, ...
+                                        startDepth, depth, eligibleAttribs, ...
+                                        mode(trainingLabels), ...
+                                        isWeighted, dataWeights, ...
+                                        isRandomForest, sizeFeatureSet);
+        %store the learned decision tree
+        baggedClassifiers(baseClassIter) = learnedTreeRoot;
+
+    end
+    
+    
+    %evaluate learned decision tree on training data
+    errorCount = 0;
+    for validIter=1:size(trainingData, 1)
+        %get prediction from learn stump
+        label = baggedPrediction(baggedClassifiers, trainingData(validIter, :));
+        if label ~= trainingLabels(validIter)
+            errorCount = errorCount + 1;
+        end
+    end
+    trainErrorPc = errorCount/size(trainingData,1);
+    fprintf('\nError rate on training set is %d', trainErrorPc);
+    
+    %evaluate learned decision tree on test data
+    errorCount = 0;
+    for validIter=1:size(testData, 1)
+        %get prediction from learn stump
+        label = baggedPrediction(baggedClassifiers, testData(validIter, :));
+        if label ~= testLabels(validIter)
+            errorCount = errorCount + 1;
+        end
+    end
+    testErrorPc = errorCount/size(testData,1);
+    fprintf('\nError rate on test set is %d', testErrorPc);
+    
+
+
+
+    
+    
