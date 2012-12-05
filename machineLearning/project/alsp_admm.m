b0c4a52efee68a636023ad791423d3a5045df29a
mohit sharma <sharm163@umn.edu>
2012-12-05 00:37:23 -0600

    optimized pre processing

--- machineLearning/project/alsp_admm.m
+++ machineLearning/project/alsp_admm.m
@@ -6,27 +6,32 @@ function []  = alsp_admm(adjFileName,src_vec,dt_vec,drange)
 % drange is a vector containing the range of destination for a particular destination is dt_vec
 % lambda is the start time interval.
 
-M = csvread(adjFileName);
-adj_mat = spconvert(M);
+M = csvread('samp_teg.txt');
+%M = [1 2 1; 1 3 1; 2 4 1; 3 4 1];
+%cur_source = 1;
+%cur_dest = 4;
+
 if length(src_vec) ~= length(dt_vec)
 	disp('Error in the input. Source and Destination vectors dont match in length')
 end
 
+numNodes = max(max(M(:,1)),max(M(:,2)));
+numEdges = size(M,1);
 
+%weights vec
+weights=zeros(numEdges, 1);
 
-[r,c] = size(adj_mat);
-%Preparing the mapping for flow vector. Here each entry in the flow vector corresponds for to edge from the Adj mat.
-Edges = [];
-ecounter = 0;
-Edge_mapper = sparse(r, r);
-[rowInd, colInd, val] = find(adj_mat);
+%constrained matrix
+constrainedGraph=sparse(numNodes,numEdges);
 
-for iter = 1:length(val)
-    if val(iter) > 0
-        ecounter = ecounter + 1;
-        Edges = [Edges;[rowInd(iter),colInd(iter)]];
-        Edge_mapper(rowInd(iter), colInd(iter)) = ecounter;
-    end
+for edgeInd=1:size(M,1)
+    %TODO: remove curr wt check if in matrix, we remove the '-1' vals
+    currWt = M(edgeInd, 3);
+    fromNode = M(edgeInd, 1); 
+    toNode = M(edgeInd, 2); 
+    constrainedGraph(fromNode, edgeInd) = 1;
+    constrainedGraph(toNode, edgeInd) =-1; 
+    weights(edgeInd) = currWt;
 end
 
 for iter = 1: length(src_vec)
@@ -37,35 +42,16 @@ for iter = 1: length(src_vec)
 	%Preparing the vector of desination nodes 
 	for k = 1:drange-1
 		cur_dest(k+1) = cur_dest(k) + 1; 	
-	end
-
-	%Code for creating the A matrix. A is the flow constraint matrix with r rows and ecounter cols
-	if r ~= c
-		disp('Error in creating of Adj matrix. It is not square');	
-	end
-
-	A = sparse(r, ecounter);
-	for node = 1:r  
-		temp_cons = zeros(1,ecounter);
-		out_edges = Edge_mapper(node, Edge_mapper(node,:) ~= 0); 		 
-		in_edges =  Edge_mapper(Edge_mapper(:,node) ~= 0, node);
-		temp_cons(out_edges) = 1;
-		temp_cons(in_edges) = -1;
-		A(node, :) = temp_cons;
-	end
-
-	b = zeros(r,1);
+    end
+    
+	b = zeros(numNodes,1);
 	b(cur_source) = length(cur_dest); 
 	b(cur_dest) = -1;
 	
 	%Calling ADMM module to find the flow
     
     %initialize weights of flow edges
-    w = zeros(ecounter, 1);
-    for edgeIter=1:length(Edges)
-        fromToPair = Edges(edgeIter, :);
-        w(edgeIter) = adj_mat(fromToPair(1), fromToPair(2));
-    end
+    w = weights;
     
     %accelaerated admm parameter b/w 1 and 1.8
     alpha = 1;
