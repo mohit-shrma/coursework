d3d5cafb3657d624d5dc14917278b4fac4a789a9
mohit sharma <mohit.786.sharma@gmail.com>
2013-03-03 14:37:02 -0600

    added timers per chunk

--- parallelComputing/hw2/serial/sd.c
+++ parallelComputing/hw2/serial/sd.c
@@ -267,11 +267,17 @@ void ComputeNeighbors(params_t *params)
     pthread_create(&p_threads[i], &attr, findSimilarDoc, (void *) &threadData[i]);
   }
 
+  gk_clearwctimer(params->timer_2);
+  gk_startwctimer(params->timer_2);
 
+  gk_clearwctimer(params->timer_4);
+  gk_startwctimer(params->timer_4);
+
+  gk_clearwctimer(params->timer_3);
+  gk_startwctimer(params->timer_3);
+  
   //wait for threads to complete, and assign the next job
   for (i=0; i < mat->nrows; i++) {
-    gk_clearwctimer(params->timer_2);
-    gk_startwctimer(params->timer_2);
     //wait for all threads to be idle before giving them work
     pthread_mutex_lock(&lockCurrentlyIdle);
     while (currentlyIdle != NTHREADS) {
@@ -316,8 +322,7 @@ void ComputeNeighbors(params_t *params)
     }
     gk_stopwctimer(params->timer_2);
 
-    gk_clearwctimer(params->timer_3);
-    gk_startwctimer(params->timer_3);
+
     //sort the hits
     if (params->nnbrs == -1 || params->nnbrs >= tempHitCount) {
       //all similarities required or 
@@ -330,8 +335,7 @@ void ComputeNeighbors(params_t *params)
     }
     gk_stopwctimer(params->timer_3);
 
-    gk_clearwctimer(params->timer_4);
-    gk_stopwctimer(params->timer_4);
+
     /* write the results in the file */
     if (fpout) {
       //fprintf(fpout, "%8d %8d\n", i, nsim);
