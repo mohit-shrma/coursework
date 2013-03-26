d1085dfc9d6a9278c4c8e350fbf8bf1d878ec3df
mohit sharma <mohit.786.sharma@gmail.com>
2013-03-26 02:36:30 -0500

    added block scanning for non power of 2 procs

--- parallelComputing/hw3/scan.c
+++ parallelComputing/hw3/scan.c
@@ -111,7 +111,7 @@ int isPowerOf2(int num) {
 int myMPIScan(int *arr, int count) {
 
   unsigned int numProcs, myRank, newRank;
-  int nearPow, tempNumProcs, procsNeeded;
+  int nearPow, tempNumProcs, procsNeeded, remProcs;
   int  *dupArr;
   int i, j, tag, status;
   
@@ -159,9 +159,13 @@ int myMPIScan(int *arr, int count) {
     //temp group finished sweep scan operations
     
     //need to perform scan on remaining procs
-    
-    //number of processors to borrow 
-    procsNeeded = tempNumProcs - ( numProcs - tempNumProcs) ;
+    remProcs = numProcs - tempNumProcs;
+
+    //find the nearest power number of processors to borrow 
+    //procsNeeded = tempNumProcs - ( numProcs - tempNumProcs) ;
+    nearPow = (int)log2(remProcs);
+    procsNeeded = pow(2, nearPow+1) - remProcs;
+
     
     //dup the result for procs that will be borrowed
     //procs with rank < procsNeeded will be borrowed
@@ -267,6 +271,196 @@ int myMPIScan(int *arr, int count) {
   return 1;
 }
 
+void getBlockSizes(int* blockSizes, int numProcs) {
+  int blockInd = 0;
+  while (numProcs != 0) {
+    blockSizes[blockInd] = pow(2, (int)log2(numProcs));
+    numProcs -= blockSizes[blockInd];
+    //printf("\nblockSizes[%d] = %d", blockInd, blockSizes[blockInd]);
+    blockInd++;
+  }
+}
+
+
+int myMPIScanMix(int *arr, int count) {
+
+  unsigned int numProcs, myRank, newRank;
+  int nearPow, tempNumProcs, procsNeeded, remProcs;
+  int  *dupArr, *blockSizes, *bcast3;
+  int i, j, tag, status, numBlocks, temp;
+  int blockRank, blockSize;
+  int blockRank3, blockSize3;
+  MPI_Comm newComm, newComm2, newComm3;
+  
+  int color, key, maxColor;
+  int color2, key2;
+  int color3, key3;
+
+  dupArr = bcast3 = blockSizes = (int*) 0;
+  tag = 100;
+  
+  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+
+  if (isPowerOf2(numProcs)) {
+    //printf("\n num of procs is power of 2");
+    mySweepMPIScan(arr, count, MPI_INT, MPI_COMM_WORLD);
+  } else {
+    //num of processes not power of 2 
+    //passed num procs not power of 2 
+    //get lower nearest power of 2
+    nearPow = (int)log2(numProcs);
+
+    //number of possible blocks
+    numBlocks = nearPow + 1;
+
+    blockSizes = (int *)malloc(sizeof(int)*numBlocks);
+    memset(blockSizes, 0, sizeof(int)*numBlocks);
+    //get block sizes 
+    getBlockSizes(blockSizes, numProcs);
+    
+    //assign color and key to each group
+    maxColor = 0;
+    j = 0;
+    temp = 0;
+    for (j=0; j < numBlocks && blockSizes[j] > 0; j++) {
+      if (myRank >= temp) {
+	color = maxColor;
+	key = myRank;
+      }
+      temp += blockSizes[j];
+      maxColor++;
+    }
+    //printf("\nmaxColor = %d", maxColor);
+    //printf("\ncolor1 myRank:%d color:%d key:%d", myRank, color, key);
+
+    //split into multiple groups according to color and key
+    MPI_Comm_split(MPI_COMM_WORLD, color, key, &newComm);
+
+    //perform mpi sweep on each group
+    mySweepMPIScan(arr, count, MPI_INT, newComm);
+
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+
+    /*for (j = 0; j< count; j++) {
+      printf("\n b4 ser scan myrank: %d arr[%d]=%d", myRank, j, arr[j]);
+      }*/
+
+
+    //get block sizes and rank
+    MPI_Comm_size(newComm, &blockSize); 
+    MPI_Comm_rank(newComm, &blockRank);
+    
+    //assign last procs of each block to dame group
+    if (blockRank == blockSize - 1) {
+      color2 = 78;
+      key2 = myRank;
+    } else {
+      color2 = 101;
+      key2 = myRank;
+    } 
+
+    //printf("\ncolor2 myRank:%d color2:%d key2:%d", myRank, color2, key2);
+    
+    //split into new groups based on color2, key2
+    MPI_Comm_split(MPI_COMM_WORLD, color2, key2, &newComm2);
+    if (color2 == 78) {
+      //dup the arr to perform sweep
+      dupArr = (int*)malloc(sizeof(int)*count);
+      memcpy(dupArr, arr, sizeof(int)*count);
+      //perform sweep on these group arr serially
+      mySimpleMPIScan(arr, dupArr, count, MPI_INT, newComm2);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    /*for (j = 0; j< count; j++) {
+      printf("\n ser scan myrank: %d arr[%d]=%d", myRank, j, arr[j]);
+      }*/
+
+    temp = 0;
+
+
+    //parse by old groups
+    for (i = 1; i < maxColor; i++, temp++) {
+
+      //assign default colors as this, procs in this group will not be used
+      color3 = 96;
+      key3 = myRank;
+
+      //get the block color whose last proc will be added
+      if (color == i-1 && color2 == 78) {
+	color3 = temp;  
+	key3 = 0;
+      }
+      
+      //get the blocks to add this second scan sum
+      if (color == i) {
+	color3 = temp;  
+	key3 = myRank + 10;
+      }
+      
+
+      //split into new groups based on color3, key3
+      MPI_Comm_split(MPI_COMM_WORLD, color3, key3, &newComm3);
+
+      //get block ranks
+      MPI_Comm_rank(newComm3, &blockRank3);
+
+      //broadcast values from lowest rank among group to all and add
+      if (color3 != 96) {
+
+	//printf("\ncolor3 myRank:%d color3:%d key3:%d", myRank, color3, key3);
+	//printf("\n myrank = %d block3Rank=%d", myRank, blockRank3);
+
+	//allocate arr to bcast
+	if (!bcast3) {
+	  bcast3 = (int *)malloc(sizeof(int)*count);
+	}
+	memset(bcast3, 0, sizeof(int)*count);
+	if (blockRank3 == 0) {
+	//lowest rank proc containinfg the second scan sum
+	  memcpy(bcast3, dupArr, sizeof(int)*count);
+	}
+	
+	//do broad cast from lowest rank
+	MPI_Bcast(bcast3, count, MPI_INT, 0, newComm3);
+	
+	//add the broadcasted array to self
+	if (blockRank3 != 0) {
+	  for (j = 0 ; j < count; j++) {
+	    //printf("\n myrank = %d block3Rank=%d adding bcast3[%d]=%d to arr[%d]=%d", \
+	    //myRank, blockRank3, j, bcast3[j], j, arr[j]);
+	    arr[j] += bcast3[j];
+	  }
+	}
+	
+	fflush(stdout);
+      }
+
+    }
+    
+  } 
+
+  //printf("\nbefore free mem");
+  if (dupArr != (int *)0) {
+    free(dupArr);
+  }
+
+  if (blockSizes != (int *)0) {
+    free(blockSizes);
+  }
+  
+  if (bcast3 != (int *)0) {
+    free(bcast3);
+  }
+  
+  return 1;
+}
+
+
 
 /*
  *Apply sweep scan across all processor on send[i]'s 
@@ -288,6 +482,11 @@ int mySweepMPIScan(int *arr, int count, MPI_Datatype datatype, MPI_Comm comm) {
   
   tag = 100;
   
+  if (numProcs == 1) {
+    //single processor no need to sweep
+    return 1;
+  }
+
   assert(isPowerOf2(numProcs));
 
   //printf("\n Number of procs: %d", numProcs);
@@ -509,18 +708,17 @@ int mySweepMPIScan(int *arr, int count, MPI_Datatype datatype, MPI_Comm comm) {
   
 
 //implements custom mpi scan
-int mySimpleMPScan(int *send, int *recv, int count, MPI_Datatype datatype,\
-	       MPI_Op op, MPI_Comm comm) {
+int mySimpleMPIScan(int *send, int *recv, int count, MPI_Datatype datatype,\
+	        MPI_Comm comm) {
 
   unsigned int numProcs, myRank;
   int tag, i;
   MPI_Status status;
-  MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
-  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+  MPI_Comm_size(comm, &numProcs);
+  MPI_Comm_rank(comm, &myRank);
   tag = 100;
-
-
-  //printf("From process %d out of %d, Hello custom scan!\n", myRank, numProcs);
+  
+   //printf("From process %d out of %d, Hello custom scan!\n", myRank, numProcs);
 
   if (myRank == 0) {
     
@@ -579,7 +777,7 @@ int main(int argc, char *argv[]) {
   //printf("From process %d out of %d, Hello World!\n", myRank, numProcs);
 
   if (argc == 2) {
-    printf("%s\n", argv[0]);
+    //printf("%s\n", argv[0]);
     //printf("\n i/p file not passed, will randomly generate inputs\n");
     numLines = atoi(argv[1]);
     fillWithRandom(&nums, numLines);
@@ -598,9 +796,9 @@ int main(int argc, char *argv[]) {
   resDup = (int *) malloc(sizeof(int) * numLines);
  
  
-  if (myRank == 0) {
-    //printf("\n displaying input arrays:");
-  }
+  /*if (myRank == 0) {
+    printf("\n displaying input arrays:");
+    }*/
 
   //make sure every process reach this checkpoint
   MPI_Barrier(MPI_COMM_WORLD);
@@ -640,7 +838,7 @@ int main(int argc, char *argv[]) {
   //myMPI_Scan(nums, resDup, numLines, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
   //myMPI_Collective_Scan(nums, resDup, numLines, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
   //mySweepMPIScan(resDup, numLines, MPI_INT, MPI_COMM_WORLD);
-  myMPIScan(resDup, numLines);
+  myMPIScanMix(resDup, numLines);
 
   //make sure every process reach this checkpoint
   MPI_Barrier(MPI_COMM_WORLD);
