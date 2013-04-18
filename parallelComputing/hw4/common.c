fe8036e9835c1643e001061090c5a1592c011479
mohit sharma <mohit.786.sharma@gmail.com>
2013-04-17 21:11:02 -0500

    added missing "coommon.c" file

---
+++ parallelComputing/hw4/common.c
@@ -0,0 +1,31 @@
+#include "common.h"
+#include <stdlib.h>
+
+void init(BVecComParams *bVecParams) {
+  if (bVecParams != (BVecComParams*) 0) {
+    bVecParams->numToSendProcs = -1;
+    bVecParams->toSendProcs = (int *) 0;
+    bVecParams->numToRecvProcs = -1;
+    bVecParams->toRecvProcs = (int *) 0;
+    bVecParams->sendPtr = (int *) 0;
+    bVecParams->sendInd = (int *) 0;
+    bVecParams->recvPtr = (int *) 0;
+    bVecParams->recvInd = (int *) 0;
+    bVecParams->sendBuf = (int *) 0;
+    bVecParams->recvBuf = (int *) 0;
+  }
+}
+
+
+void freeBVecComParams(BVecComParams *bVecParams) {
+  if (bVecParams != (BVecComParams*) 0) {
+    free(bVecParams->toSendProcs);
+    free(bVecParams->toRecvProcs);
+    free(bVecParams->sendPtr);
+    free(bVecParams->sendInd);
+    free(bVecParams->recvPtr);
+    free(bVecParams->recvInd);
+    free(bVecParams->sendBuf);
+    free(bVecParams->recvBuf);
+  }
+}
